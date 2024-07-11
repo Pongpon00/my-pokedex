9a071eb45da8aade46aec3f03b0919007da90638
Pongpon Butsrimart <wavenakab2019@gmail.com>
2024-07-11 16:38:21 +0700

    ✨ feat | hide navbar when scroll

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,22 +1,51 @@
 import Pokeball from "../assets/pokeball.png";
 import Pokemon_logo from "../assets/International_Pokémon_logo.png";
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 
-function Navbar(){
-    return (
-        <>
-          <div className=" fixed top-0 left-0 flex flex-row bg-rose-500 w-full items-center h-14 px-6 text-white shadow-sm z-50">
-            <div className="flex w-full h-full items-center justify-start">
-                <img className="size-7" src={Pokeball} alt="" />
-                <img className="px-2 h-7" src={Pokemon_logo} alt="" />
-            </div>
-            <div className="flex w-full h-full items-center justify-end">
-                <Link className="px-8" to={"/"}>Pokedex</Link>
-                <Link to={"/favourite"} className="">My Favourite</Link>
-            </div>
-          </div>
-        </>
-      );
-}
+const Navbar = () => {
+  const [hideNavbar, setHideNavbar] = useState(false);
+  let prevScrollpos:number = window.scrollY;
+
+  const handleScroll = () => {
+    const currentScrollPos = window.scrollY;
+    if (prevScrollpos > currentScrollPos) {
+      setHideNavbar(false);
+    } else {
+      setHideNavbar(true);
+    }
+    prevScrollpos = currentScrollPos;
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <>
+      <div
+        className={`fixed top-0 left-0 flex flex-row bg-rose-500 w-full items-center h-14 px-6 text-white shadow-sm z-50 transition-transform duration-300 ${
+          hideNavbar ? "-translate-y-full" : "translate-y-0"
+        }`}
+      >
+        <div className="flex w-full h-full items-center justify-start">
+          <img className="size-7" src={Pokeball} alt="Pokeball" />
+          <img className="px-2 h-7" src={Pokemon_logo} alt="Pokemon Logo" />
+        </div>
+        <div className="flex w-full h-full items-center justify-end">
+          <Link className="px-8" to={"/"}>
+            Pokedex
+          </Link>
+          <Link to={"/favourite"} className="">
+            My Favourite
+          </Link>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Navbar;
